efc27839c542436a3f5b95d30d40b2b79a9770cc
Ben Lagnese <93357107+BenLagneseUMBC@users.noreply.github.com>
2022-05-09 14:21:20 -0400

    Testing

--- back-end/test.py
+++ back-end/test.py
@@ -1,7 +1,7 @@
 # Test file for running API functions (as running code directly within those files can make imports a nightmare).
 # It is otherwise safe to remove. 
 
-import back_end_api
+import back_end_api.frontend_funcs as front_funcs
 import back_end_api.internal_funcs as in_funcs
-result = in_funcs.in_get_column('census_tract')
+result = front_funcs.get_column('census_tract')
 print(result)
