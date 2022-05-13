5258444281074a515c0dc9ef62cc288f8023f710
Ben Lagnese <93357107+BenLagneseUMBC@users.noreply.github.com>
2022-05-13 17:22:01 -0400

    Update example api

--- pages/api/cluster/[id].js
+++ pages/api/cluster/[id].js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     const data = await db.all(
-      'SELECT CrimeLocation.id, offense_key, method FROM CrimeLocation JOIN CrimeData ON CrimeData.id = CrimeLocation.id WHERE neighborhood_cluster = ?',
+      'SELECT CrimeLocation.location_id, offense_key, method FROM CrimeLocation JOIN CrimeData ON CrimeData.location_id = CrimeLocation.location_id WHERE neighborhood_cluster = ?',
       [`cluster ${req.query.id}`]
     )
     res.status(200).json(data)
