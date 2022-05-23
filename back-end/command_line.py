0f4b95c7691da0a32e75975ef4d65c3993dce12a
Dinorush <onparnorthstar@gmail.com>
2022-05-22 21:18:12 -0400

    Remove deprecated back-end files

--- back-end/command_line.py
+++
@@ -1,8 +0,0 @@
-from back_end_api import frontend_funcs
-import sys
-
-func_to_call = getattr(frontend_funcs, sys.argv[1])
-if len(sys.argv) == 3:
-	print(func_to_call(sys.argv[2]))
-else:
-	print(func_to_call(sys.argv[2:]))
--- back-end/test.py
+++
@@ -1,7 +0,0 @@
-# Test file for running API functions (as running code directly within those files can make imports a nightmare).
-# It is otherwise safe to remove. 
-
-import back_end_api.frontend_funcs as front_funcs
-import back_end_api.internal_funcs as in_funcs
-result = front_funcs.get_column('census_tract')
-print(result)
